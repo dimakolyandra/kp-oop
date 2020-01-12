0f386f63ff0509ae453e78d3d9bfa3b3e47c3b6c
Никита Трубченко <nikitatrubcenko@MacBook-Pro-Nikita.local>
2020-01-12 10:48:55 +0300

    add orders list

---
+++ client/src/components/OrderIcon.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="container">
+		<div class='flex-item'>
+			<div class='icon'>
+				<div><span style="font-size: 30px; font-family: Avenir Next; font-weight: 700; color: rgb(30, 30, 30)">{{id}}</span></div>
+				<div><span style="font-size: 20px; font-family: Avenir Next; font-weight: 700; color: rgb(30, 30, 30)">{{name}}</span></div>
+			</div>
+		</div>
+		<div class='flex-item'>
+			<div class='container-col'>
+				<div class='flex-item-icon'><span style="background-color: rgb(149,188,106); font-size: 20px; font-family: Avenir Next; font-weight: 700; color: white">Accept</span></div>
+				<div class='flex-item-icon'><span style="background-color: rgb(206,60,60); font-size: 20px; font-family: Avenir Next; font-weight: 700; color: white">Cancel</span></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "OrderIcon",
+		props: {
+			id: Number,
+			name: String
+		}
+	}
+</script>
+
+<style scoped>
+.icon {
+	background-color: rgb(169,143,80);
+	height: 150px;
+	width: 300px;
+	text-align: center;
+}
+.container {
+  display: flex;
+  flex-direction: row
+}
+.flex-item {
+	flex: 0.1;
+}
+.flex-item:nth-of-type(2) {
+	margin-left:2%;
+}
+
+.container-col {
+	display: flex;
+	flex-direction: column;
+}
+.flex-item-icon {
+	margin-top:30%;
+	flex: 1;
+	padding: 5px;
+}
+
+</style>
---
+++ client/src/components/OrdersList.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+	<div class="container">
+		<div class="flex-item">
+			<img src="../assets/logo.png" width=100%>
+		</div>
+		<div class="flex-item">
+			<span style="background-color: rgb(206,60,60); font-size: 50px; font-family: Avenir Next; font-weight: 700; color: white">Orders list</span>
+		</div>
+	</div>
+	<div class="container-col">
+		<div class="flex-item-icon" v-for="order in data" v-bind:key="order"><OrderIcon :id="order.order_id" :name="order.order_items"></OrderIcon></div>
+	</div>
+</div>
+</template>
+
+<script>
+	import OrderIcon from './OrderIcon.vue'
+	export default {
+		name: 'OrdersList',
+		components: {
+			OrderIcon
+		},
+		data: function(){
+			return {
+				data: [ 
+					{ 
+						"order_id": 5, 
+						"order_items": "10 огурцов; 12 помидоров" 
+					}, 
+					{ 
+						"order_id": 6, 
+						"order_items": "10 огурцов; 12 помидоров" 
+					}, 
+					{ 
+						"order_id": 7, 
+						"order_items": "10 огурцов; 12 помидоров" 
+					} 
+					]
+			}
+		}
+	}
+</script>
+
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row
+}
+.flex-item {
+	flex: 1;
+}
+.flex-item:nth-of-type(2) {
+	margin-top:14%;
+	margin-left:2%;
+}
+.flex-item:nth-of-type(1) {
+	flex: 0.1;
+	margin-top:10%;
+	margin-left: 10%;
+}
+.container-col {
+	display:flex;
+	flex-direction: column;
+}
+.flex-item-icon {
+	flex: 1;
+	margin-left: 30%;
+	padding: 5px;
+}
+</style>
+
+
+
+
